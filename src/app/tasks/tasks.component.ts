b3b612d8a72c860ae29bf3123f44c9c472c5e7c3
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2019-01-06 11:35:17 +0100

    Working Task tables

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import data from '../../db/tasks.json';
+import { Observable } from 'rxjs';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-tasks',
@@ -8,13 +10,42 @@ import data from '../../db/tasks.json';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks;
+  all_tasks = new Array;
+  tasks = new Array;
   project;
   not_empty = true;
 
-  constructor(private route: ActivatedRoute) {
-    this.tasks = data;
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+    this.getJSON();
     this.route.params.subscribe(params => { this.project = params['project']; });
+  }
+
+  getApi() : Observable<any> {
+    return this.http.options("http://localhost/Laboweb/public/index.php/api/tasks");
+  }
+
+  getJSON() {
+    this.getApi().subscribe(data => {
+      this.all_tasks = data;
+      this.trimJSON();
+      this.assertEmpty();
+    });
+  }
+
+  assertEmpty() {
+    if(this.tasks.length == 0) {
+      this.not_empty = false;
+    } else {
+      this.not_empty = true;
+    }
+  }
+
+  trimJSON() {
+    for(let task of this.all_tasks) {
+      if (task.projects.title == this.project) {
+        this.tasks.push(task);
+      }
+    }
   }
 
   ngOnInit() {
